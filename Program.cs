2e866ac79e80c0d71ecfb1a94979ab4a53ff597e
learningpath2025 <learningpath2025@outlook.com>
2025-09-02 10:45:33 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment2._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Adding two integers, Result:  {SimpleMath.Add(4, 5)}");
+            Console.WriteLine($"Adding three decimals, Result:  {SimpleMath.Add(4.5m, 5.5m, 6.5m)}");
+            Console.WriteLine($"Subtracting two integers, Result:  {SimpleMath.Subtract(10, 5)}");
+            Console.WriteLine($"Subtracting two decimals, Result:  {SimpleMath.Subtract(10.5m, 5.5m)}");
+            Console.WriteLine($"Multiplying two integers, Result:  {SimpleMath.Multiply(4, 5)}");
+            Console.WriteLine($"Multiplying two floats, Result:  {SimpleMath.Multiply(4.5f, 5.5f)}");
+            Console.WriteLine($"Multiplying three floats, Result:  {SimpleMath.Multiply(4.5f, 5.5f, 6.5f)}");
+            Console.WriteLine($"Dividing two integers, Result:  {SimpleMath.Divide(10, 5)}");
+        }
+    }
+}
---
+++ SimpleMath.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2._2._2
+{
+    internal class SimpleMath
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static decimal Add(decimal a, decimal b, decimal c)
+        {
+            return a + b + c;
+        }
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static decimal Subtract(decimal a, decimal b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+        public static float Multiply(float a, float b, float c)
+        {
+            return a * b * c;
+        }
+        public static double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Denominator cannot be zero.");
+            }
+            return (double)a / b;
+        }
+    }
+}
